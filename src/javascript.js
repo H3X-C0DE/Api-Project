c7a28ca7a00e720d0d553448201e9db78671a24e
IIHEXYII <vetletessem@gmail.com>
2022-11-23 15:34:48 +0100

    Fetching id,name,types & image

--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,6 @@
 const pokeContainer = document.getElementById("pokemon-container");
-const pokemonAmount = 1154;
+// const pokemonAmount = 1154;
+const pokemonAmount = 20;
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokemonAmount; i++) {
@@ -14,14 +15,22 @@ const getPokemon = async (id) => {
   console.log(pokemon);
   createCard(pokemon);
 };
-// fetchPokemon();
+fetchPokemon();
 
 function createCard(pokemon) {
   const cardEl = document.createElement(`div`);
   cardEl.classList.add(`pokemon`);
-
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const type = pokemon.types.map((typeEl) => typeEl.type.name);
+  const id = pokemon.id;
   const cardInnerHTML = `
-  ${pokemon.id}
+  <div class="img-container">
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+  <div class="info">
+  <span class=""number>${id}</span></div>
+  <h3 class="name">${name}</h3>
+  <h4 class="type">Type: <span>${type}</span></h4>
+  </div>
   `;
   cardEl.innerHTML = cardInnerHTML;
   pokeContainer.append(cardEl);
