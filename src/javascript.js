8ff080c7f42f98c3289875cf782a9d7867dfcd25
IIHEXYII <vetletessem@gmail.com>
2022-11-23 16:06:19 +0100

    added: CSS Styling 🎨

--- src/javascript.js
+++ src/javascript.js
@@ -1,6 +1,23 @@
 const pokeContainer = document.getElementById("pokemon-container");
 // const pokemonAmount = 1154;
 const pokemonAmount = 20;
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+const mainTypes = Object.keys(colors);
 
 const fetchPokemon = async () => {
   for (let i = 1; i <= pokemonAmount; i++) {
@@ -18,16 +35,28 @@ const getPokemon = async (id) => {
 fetchPokemon();
 
 function createCard(pokemon) {
+  //creates the card element
   const cardEl = document.createElement(`div`);
+  // then Adds a class tag to it
   cardEl.classList.add(`pokemon`);
+  // adds a variable with each name starts with a Upper case.
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-  const type = pokemon.types.map((typeEl) => typeEl.type.name);
-  const id = pokemon.id;
+  //  Goes through the array of types
+  const pokemonTypes = pokemon.types.map((typeEl) => typeEl.type.name);
+  // and adds the common type of the pokemon
+  const type = mainTypes.find((type) => pokemonTypes.indexOf(type) > -1);
+  const color = colors[type];
+  cardEl.style.backgroundColor = color;
+  // adds a series of zero to the pokemon ID
+  const pokeNumber = pokemon.id.toString().padStart(4, "0");
+
+  // generates the HTML element
   const cardInnerHTML = `
   <div class="img-container">
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
+  </div>
   <div class="info">
-  <span class=""number>${id}</span></div>
+  <span class="number">#${pokeNumber}</span></div>
   <h3 class="name">${name}</h3>
   <h4 class="type">Type: <span>${type}</span></h4>
   </div>
