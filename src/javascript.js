03db130de831ee78961333da139c5e03fc958a1f
IIHEXYII <vetletessem@gmail.com>
2022-11-23 15:02:35 +0100

    fetches api data

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,28 @@
-console.log("Js Connected!");
+const pokeContainer = document.getElementById("pokemon-container");
+const pokemonAmount = 1154;
+
+const fetchPokemon = async () => {
+  for (let i = 1; i <= pokemonAmount; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  console.log(pokemon);
+  createCard(pokemon);
+};
+// fetchPokemon();
+
+function createCard(pokemon) {
+  const cardEl = document.createElement(`div`);
+  cardEl.classList.add(`pokemon`);
+
+  const cardInnerHTML = `
+  ${pokemon.id}
+  `;
+  cardEl.innerHTML = cardInnerHTML;
+  pokeContainer.append(cardEl);
+}
